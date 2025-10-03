01c85ae421b6e6efe90cce16509c563ff0bd45cc
Vasya-556 <vkobrin556@gmail.com>
2025-10-04 01:32:42 +0300

    feat: added event listeners to all buttons

--- src/script.ts
+++ src/script.ts
@@ -7,6 +7,12 @@ const speed_label = document.getElementById("speed-range-label") as HTMLLabelEle
 const sort_algorithm_select = document.getElementById("algorithm-select") as HTMLSelectElement;
 let sort_algorithm_name = "Block sort"
 
+const start_button = document.getElementById("start-button") as HTMLButtonElement;
+const stop_button = document.getElementById("stop-button") as HTMLButtonElement;
+const reset_button = document.getElementById("reset-button") as HTMLButtonElement;
+const generate_random_data_button = document.getElementById("generate-random-data-button") as HTMLButtonElement;
+const generate_pseudorandom_data_button = document.getElementById("generate-pseudorandom-data-button") as HTMLButtonElement;
+
 size_input.addEventListener("input", () => {
     size_label.textContent = `size: ${size_input.value} elements`;
 });
@@ -17,6 +23,26 @@ speed_input.addEventListener("input", () => {
 
 sort_algorithm_select.addEventListener("change", () => {
     sort_algorithm_name = sort_algorithm_select.value;
+});
+
+start_button.addEventListener("click", () => {
+    console.log(start_button.textContent);
+});
+
+stop_button.addEventListener("click", () => {
+    console.log(stop_button.textContent);
+});
+
+reset_button.addEventListener("click", () => {
+    console.log(reset_button.textContent);
+});
+
+generate_random_data_button.addEventListener("click", () => {
+    console.log(generate_random_data_button.textContent);
+});
+
+generate_pseudorandom_data_button.addEventListener("click", () => {
+    console.log(generate_pseudorandom_data_button.textContent);
 });
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
