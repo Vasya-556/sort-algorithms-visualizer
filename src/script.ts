493fe004ad2db63294e4d1abc9e7bad0111fb3e1
Vasya-556 <vkobrin556@gmail.com>
2025-10-03 20:49:20 +0300

    feat: added listener to sort algorithm selector and wrote skelton for result panel and canvas

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,28 @@
 const size_input = document.getElementById("size-range") as HTMLInputElement;
 const size_label = document.getElementById("size-range-label") as HTMLLabelElement;
+
 const speed_input = document.getElementById("speed-range") as HTMLInputElement;
 const speed_label = document.getElementById("speed-range-label") as HTMLLabelElement;
+
+const sort_algorithm_select = document.getElementById("algorithm-select") as HTMLSelectElement;
+let sort_algorithm_name = "Block sort"
 
 size_input.addEventListener("input", () => {
     size_label.textContent = `size: ${size_input.value} elements`;
 });
 
-
 speed_input.addEventListener("input", () => {
     speed_label.textContent = `speed: ${speed_input.value} ms`;
 });
+
+sort_algorithm_select.addEventListener("change", () => {
+    sort_algorithm_name = sort_algorithm_select.value;
+});
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+canvas.style.background = "#ff0000";
+let window_height = window.innerHeight;
+let window_width = window.innerWidth;
+
+canvas.width = window_width;
+canvas.height = window_height;
