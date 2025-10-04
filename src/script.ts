0887d35fa9235ecb4fb3fa4f633a9a2dbf9634b2
Vasya-556 <vkobrin556@gmail.com>
2025-10-04 20:39:50 +0300

    feat: wrote generate random and pseudo random data functions and set canvas color, resize properties

--- src/script.ts
+++ src/script.ts
@@ -10,11 +10,16 @@ let sort_algorithm_name = "Block sort"
 const start_button = document.getElementById("start-button") as HTMLButtonElement;
 const stop_button = document.getElementById("stop-button") as HTMLButtonElement;
 const reset_button = document.getElementById("reset-button") as HTMLButtonElement;
+const shuffle_button = document.getElementById("shuffle-button") as HTMLButtonElement;
 const generate_random_data_button = document.getElementById("generate-random-data-button") as HTMLButtonElement;
 const generate_pseudorandom_data_button = document.getElementById("generate-pseudorandom-data-button") as HTMLButtonElement;
 
+let size: number = Number(size_input.value); 
+let data: number[] = [];
+
 size_input.addEventListener("input", () => {
     size_label.textContent = `size: ${size_input.value} elements`;
+    size = Number(size_input.value);
 });
 
 speed_input.addEventListener("input", () => {
@@ -27,6 +32,7 @@ sort_algorithm_select.addEventListener("change", () => {
 
 start_button.addEventListener("click", () => {
     console.log(start_button.textContent);
+    console.log(data);
 });
 
 stop_button.addEventListener("click", () => {
@@ -37,12 +43,18 @@ reset_button.addEventListener("click", () => {
     console.log(reset_button.textContent);
 });
 
+shuffle_button.addEventListener("click", () => {
+    console.log(shuffle_button.textContent);
+});
+
 generate_random_data_button.addEventListener("click", () => {
-    console.log(generate_random_data_button.textContent);
+    // console.log(generate_random_data_button.textContent);
+    generate_random_data();
 });
 
 generate_pseudorandom_data_button.addEventListener("click", () => {
-    console.log(generate_pseudorandom_data_button.textContent);
+    // console.log(generate_pseudorandom_data_button.textContent);
+    generate_pseudorandom_data();
 });
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -52,3 +64,38 @@ let window_width = window.innerWidth;
 
 canvas.width = window_width;
 canvas.height = window_height;
+
+const ctx = canvas.getContext("2d")
+
+if (!ctx) {
+    throw new Error("Could not get 2D context");
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+const generate_random_data = () => {
+    data = Array.from({ length: size }, () => Math.floor(Math.random() * size) + 1);
+}
+
+generate_random_data()
+const generate_pseudorandom_data = () => {
+    data = Array.from({ length: size}, (_, i) => i + 1)   
+    shuffle_data()
+}
+
+const shuffle_data = () => {
+    let m = size, t, i;
+
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+
+        t = data[m];
+        data[m] = data[i];
+        data[i] = t;
+    }
+}
